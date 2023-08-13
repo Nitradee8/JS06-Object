79017c67ea3841164d6a39b164c659270424c3a4
Nitradee8 <nbtt.nik@gmail.com>
2023-08-13 21:35:58 +0700

    update

--- 02-Loop/04-theSentinel/main.js
+++ 02-Loop/04-theSentinel/main.js
@@ -1,3 +1,16 @@
 // ให้เขียนโปรแกรมที่รับค่า key และ value ของ Properties ของ Object หนึ่ง จนกว่าจะเจอคำว่า stop ถึงหยุด   
 // และให้นำค่าเหล่านั้นมาสร้าง Object หลังจากนั้น log object นั้นออกมา
 
+const Object1 = {};
+
+while (true) {
+  const key = prompt('Enter a key (or "stop" to finish):');
+  
+  if (key === 'stop') {
+    break;
+  }
+  const value = prompt(`Enter the value for key "${key}":`);
+  newObject[key] = value;
+}
+
+console.log('Final Object:', Object1);
