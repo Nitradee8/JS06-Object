14250f7236fb25d5c2666899526cdd3b201230cc
Nitradee8 <nbtt.nik@gmail.com>
2023-08-13 22:44:19 +0700

    update

--- 06-Constructor/02/main.js
+++ 06-Constructor/02/main.js
@@ -2,3 +2,20 @@
 // - มี property ชื่อ currentValue ไว้เก็บค่าสะสม (มีค่าเท่ากับ startingValue ตอน Object ถูกสร้าง)
 // -  มี method  read() ให้รับค่า input และให้บวกเข้ากับค่า currentValue
 // -  มี method  show() ให้ alert ค่า currentValue
+
+function Accumulator(startingValue) {
+    this.currentValue = startingValue;
+  
+    this.read = function() {
+      const inputValue = +prompt('Enter a value to add:');
+      this.currentValue += inputValue;
+    };
+  
+    this.show = function() {
+      alert(`Current Value: ${this.currentValue}`);
+    };
+  }
+  
+  const accumulator = new Accumulator(0);
+  accumulator.read();
+  accumulator.show();
