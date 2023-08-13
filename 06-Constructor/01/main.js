00eee15808e71aacda83d83e6b02ec7238d94113
Nitradee8 <nbtt.nik@gmail.com>
2023-08-13 22:40:15 +0700

    update

--- 06-Constructor/01/main.js
+++ 06-Constructor/01/main.js
@@ -1,35 +1,30 @@
-// - ให้สร้าง Object calculator ซึ่งมี 3 method 
-// - read() ให้รับค่า input 2 ค่า
-// - sum() ให้ return ค่าผลบวกของเลขทั้งสอง
-// - mul() ให้ return ค่าผลคูณของเลขทั้งสอง
-
-let calculator = {
-    read() {
-      this.x = +prompt('Enter num1');
-      this.y = +prompt('Enter num2');
-      },
-      sum() {
-          return this.x + this.y;
-        },
-        mul() {
-        return this.x * this.y;
-      },
-    };
-  
-    log(calculator);
-  
-  calculator.read();
-  alert(calculator.sum());
-  alert(calculator.mul());
-
   // ให้สร้าง Object calculator ซึ่งมี 3 method  
   // (ให้ใช้วิธี Constructor Function)
   // - read() ให้รับค่า input 2 ค่า
   // - sum() ให้ return ค่าผลบวกของเลขทั้งสอง
   // - mul() ให้ return ค่าผลคูณของเลขทั้งสอง
   
+function Calculator() {
+  this.read = function() {
+    this.x = +prompt('Enter num1');
+    this.y = +prompt('Enter num2');
+  };
   
-log('Constructor Function');
+  this.sum = function() {
+    return this.x + this.y;
+  };
+  
+  this.mul = function() {
+    return this.x * this.y;
+  };
+}
+
+let calculator = new Calculator();
+
+console.log(calculator);
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
 
 
 
